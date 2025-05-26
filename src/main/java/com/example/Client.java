35a35ca3a4a908f2296cf116c50ddf07853258dc
tinleviet <10422078@student.vgu.edu.vn>
2025-05-26 17:25:19 +0700

    Update Client.java to allow user input for host address and port number

--- src/main/java/com/example/Client.java
+++ src/main/java/com/example/Client.java
@@ -1,13 +1,23 @@
 package com.example;
 
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try{
             // Socket s = new Socket("192.168.1.13",1234);
-            Socket s = new Socket("localhost",1234);
+
             
+            Scanner scanner = new Scanner(System.in);
+            String sc = scanner.nextLine();
+            System.out.print("Enter host address: ");
+            String host = scanner.nextLine();
+            System.out.print("Enter port number: ");
+            int port = Integer.parseInt(scanner.nextLine());
+            System.out.println("Connecting to " + host + " on port " + port);
+            Socket s = new Socket(host, port);
+
             ClientSending sending = new ClientSending(s);
             ClientReceiving receiving = new ClientReceiving(s);
 
